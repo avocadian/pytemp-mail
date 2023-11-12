3638cb223428e4015ae7bdb4ebb44ac4791189d5
---n <confucius-@tuta.io>
2023-11-12 19:01:17 +0900

    Edit setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,12 @@
 from setuptools import setup, find_packages
 
+setup(
+        name='pytemp-mail',
+        version='1.0.0',
+        packages=find_packages(),
+        description='Api of temp-mail.lol',
+        url='https://github.com/2-l0/pytemp-mail',
+        install_requires=[
+            'httpx'
+            ],
+        )
